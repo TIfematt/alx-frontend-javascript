331eafd6e0982478f7940b1650af32d7c44b6e32
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-18 22:50:00 +0100

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,7 @@ export default function handleResponseFromAPI(promise) {
     return promise
         .then(() => obj)
         .catch(() => new Error())
-        .finally(() => console.log('Got a response from the API'))
+        .finally(() => { console.log('Got a response from the API'); });
 }
 
 
