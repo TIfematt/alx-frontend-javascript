37cabd86b211fb7ceba8d82c1762447a54cadf1b
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-18 01:39:26 +0100

    Task 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,19 @@
+// task 0. Keep everything together!
+
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
-  }
-  
-  export function getLast() {
+}
+
+// get last part of the string
+export function getLast() {
     return ' is okay';
-  }
-  
-  export function taskNext() {
+}
+
+// get the whole string
+export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
-  }
+}
