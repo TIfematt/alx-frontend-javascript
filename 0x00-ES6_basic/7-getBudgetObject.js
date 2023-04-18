26a9a4ac159df2b0f3d5582d601e4469baec61c1
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-18 11:23:12 +0100

    Task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+}
+
+console.log(getBudgetObject(400, 700, 900));
