73fc5679d10f3eabe7f6c119425b6917eebfd5ef
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-18 01:37:51 +0100

    Task 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,15 @@
-// Javascript script that modifies the value of myVar to 333
-
-export const taskFirst = () => {
-    var task = 'I prefer const when I can.';
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
     return task;
-}
-
-// get last string
-export function getLast() {
-  return ' is okay';
-}
-
-// get next string
-export let taskNext= () => {
-  var combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
-
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
