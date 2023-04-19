54d31e1ebdb9e5204561f723b18c1ff9955ea5ee
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-19 11:42:18 +0100

    Task 4 and 5

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,13 +1,4 @@
 /* eslint-disable */
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-        if (firstName != null) {
-            resolve({
-                firstName,
-                lastName
-            })
-        } else {
-            reject(new Error)
-        }
-    })
+    return Promise.resolve({ firstName, lastName })
 }
