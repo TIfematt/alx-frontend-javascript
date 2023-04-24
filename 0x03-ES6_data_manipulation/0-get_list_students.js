46d96bf756f07bc9824ccae40eedc0543c6d8825
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-24 14:30:20 +0100

    Task 0 added

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,22 @@
 /*eslint-disable */
+
+export default function getListStudents () {
+    return [
+        {
+            id: 1,
+            firstName: 'Guillaume',
+            location: 'San Francisco'
+        },
+        {
+            id: 2,
+            firstName: 'James',
+            location: 'Columbia'  
+        },
+        {
+            id: 5,
+            firstName: 'Serena',
+            location: 'San Francisco'  
+        }
+    ]
+}
+
