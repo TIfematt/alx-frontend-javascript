15c03e9547ac3467602aadc9cdad2c242db2e37b
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-24 15:07:35 +0100

    Task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,14 @@
 /*eslint-disable */
+import getListStudents from "./0-get_list_students.js";
+
+export default function getListStudentIds (arr) {
+
+    if (!Array.isArray(arr)) {
+        return []
+    }
+    const ids = arr.map(id => id.id)
+    return ids;
+}
+
+console.log(getListStudentIds("hello"));
+console.log(getListStudentIds(getListStudents()));
