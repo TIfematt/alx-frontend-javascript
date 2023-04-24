ade4923e949c236df68acf6cf25dc90721e58903
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-04-24 15:59:46 +0100

    upto task 3

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,8 @@
 /*eslint-disable */
+import getListStudents from "./0-get_list_students.js";
+
+export default function getStudentsByLocation (arr, city) {
+    const getLocation = arr.filter(student => student.location === city)
+
+    return getLocation
+}
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,12 @@
 /*eslint-disable */
+import getListStudents from "./0-get_list_students.js";
+
+export default function getStudentIdsSum (arr) {
+    const initialValue = 0;
+    const ids = arr.map(id => id.id).reduce(
+        (accumulator, currectValue) => accumulator + currectValue,
+        initialValue
+    )
+    return ids
+}
+
